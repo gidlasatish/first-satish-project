9cf0a37ad76c90d2f289093d14fbff24793171ab
gidlasatish <gsatish013@gmail.com>
2022-06-15 23:50:44 -0700

    add initial commit

---
+++ src/main/java/com/satishcompany/first_satish_project/App.java
@@ -0,0 +1,13 @@
+package com.satishcompany.first_satish_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/satishcompany/first_satish_project/AppTest.java
@@ -0,0 +1,20 @@
+package com.satishcompany.first_satish_project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
